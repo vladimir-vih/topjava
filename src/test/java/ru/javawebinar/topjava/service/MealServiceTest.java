971b9223163bfd266df1ac6969f02f2873b4581d
v.vikhlyantsev <vvikhlyantsev@humans.net>
2022-11-14 09:13:23 +0300

    Lesson03. HW03 bug fixes. Added assertThrows in the tests.

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,16 +1,12 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -19,9 +15,6 @@ import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import javax.sql.DataSource;
-
-
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
@@ -29,14 +22,15 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 import static ru.javawebinar.topjava.MealTestData.*;
-import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration(locations = {
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringRunner.class)
-@Sql(scripts = {"classpath:db/initDB.sql"}, config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = {"classpath:db/initDB.sql", "classpath:db/populateDB.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
     static {
@@ -50,33 +44,22 @@ public class MealServiceTest {
     @Autowired
     private MealService service;
 
-    @Autowired
-    private DataSource dataSource;
-
-    @Before
-    public void setUp() {
-        log.info("do setUp before test");
-        Resource resource = new ClassPathResource("db/populateDB.sql");
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(resource);
-        databasePopulator.execute(dataSource);
-    }
-
     @Test
     public void getExist() {
         log.info("do getExist");
         assertMatch(service.get(ID_MEAL1_OF_USER, USER_ID), meal1User);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void getNotExist() {
         log.info("do getNotExist");
-        service.get(NOT_FOUND_MEAL_ID, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND_MEAL_ID, USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void getMealOtherUser() {
         log.info("do getNotExist");
-        service.get(ID_MEAL1_OF_ADMIN, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(ID_MEAL1_OF_ADMIN, USER_ID));
     }
 
     @Test
@@ -85,31 +68,31 @@ public class MealServiceTest {
         service.delete(ID_MEAL1_OF_USER, USER_ID);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void deleteNotExist() {
         log.info("do deleteNotExist");
-        service.delete(NOT_FOUND_MEAL_ID, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND_MEAL_ID, USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void deleteMealOtherUser() {
         log.info("do deleteMealOtherUser");
-        service.delete(ID_MEAL1_OF_ADMIN, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.delete(ID_MEAL1_OF_ADMIN, USER_ID));
     }
 
     @Test
-    public void getBetweenInclusiveEmptyResult() {
-        assertEquals(0, service.getBetweenInclusive(checkDate, checkDate, USER_ID).size());
+    public void getBetweenInclusiveTwoMealsResult() {
+        assertEquals(2, service.getBetweenInclusive(checkDate, checkDate.plusDays(1), USER_ID).size());
     }
 
     @Test
-    public void getBetweenInclusiveThreeDaysResult() {
-        assertEquals(3, service.getBetweenInclusive(checkDate, checkDate, ADMIN_ID).size());
+    public void getBetweenInclusiveThreeMealsResult() {
+        assertEquals(3, service.getBetweenInclusive(checkDate.minusDays(1), checkDate, ADMIN_ID).size());
     }
 
     @Test
     public void getAll() {
-        List<Meal> expectedList = Arrays.asList(meal3User, meal2User, meal1User);
+        List<Meal> expectedList = Arrays.asList(meal5User, meal4User, meal3User, meal2User, meal1User);
         assertEquals(expectedList, service.getAll(USER_ID));
     }
 
@@ -119,14 +102,14 @@ public class MealServiceTest {
         assertMatch(service.get(ID_MEAL3_OF_USER, USER_ID), updateMealOfUser);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void updateNotExist() {
-        service.update(notAddedMeal, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.update(notAddedMeal, USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void updateMealOfOtherUser() {
-        service.update(meal1Admin, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.update(meal1Admin, USER_ID));
     }
 
     @Test
@@ -137,11 +120,11 @@ public class MealServiceTest {
         assertMatch(createdMeal, testMeal);
     }
 
-    @Test(expected = DataAccessException.class)
+    @Test
     public void createDuplicateDate() {
         final Meal duplicatedDateMeal = MealTestData.getNew();
         duplicatedDateMeal.setDateTime(LocalDateTime.of(checkDate, LocalTime.of(14,0)));
-        service.create(duplicatedDateMeal, ADMIN_ID);
+        assertThrows(DataAccessException.class, () -> service.create(duplicatedDateMeal, ADMIN_ID));
     }
 
     @Test
