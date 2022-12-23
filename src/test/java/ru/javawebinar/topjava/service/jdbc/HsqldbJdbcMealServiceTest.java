0df11babf424b2f9d8dd51d3037b4c43fe666835
v.vikhlyantsev <vvikhlyantsev@humans.net>
2022-12-24 00:50:27 +0300

    Lesson05. HW05 bug fixes3

--- src/test/java/ru/javawebinar/topjava/service/jdbc/HsqldbJdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/HsqldbJdbcMealServiceTest.java
@@ -1,8 +1,4 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.Profiles;
-
-@ActiveProfiles(Profiles.HSQL_DB)
 public class HsqldbJdbcMealServiceTest extends JdbcMealServiceTest {
 }
--- src/test/java/ru/javawebinar/topjava/service/jdbc/PostgresJdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/PostgresJdbcMealServiceTest.java
@@ -1,8 +1,4 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.Profiles;
-
-@ActiveProfiles(Profiles.POSTGRES_DB)
 public class PostgresJdbcMealServiceTest extends JdbcMealServiceTest {
 }
